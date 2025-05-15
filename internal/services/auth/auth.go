b49137f40511b3dfe0ea76221d4e13eb46f16f07
finlleyl <ilya_chibugaev@mail.ru>
2025-05-15 08:56:52 +0300

    add Auth struct and constructor

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"errors"
 	"github.com/finlleyl/gRPC/internal/domain/models"
+	"go.uber.org/zap"
+	"time"
 )
 
 var (
@@ -20,4 +22,22 @@ type UserProvider interface {
 
 type AppProvider interface {
 	App(ctx context.Context, appID int) (models.App, error)
+}
+
+type Auth struct {
+	log         *zap.SugaredLogger
+	usrSaver    UserSaver
+	usrProvider UserProvider
+	appProvider AppProvider
+	tokenTTL    time.Duration
+}
+
+func New(log *zap.SugaredLogger, userSaver UserSaver, userProvider UserProvider, appProvider AppProvider, tokenTTL time.Duration) *Auth {
+	return &Auth{
+		log:         log,
+		usrSaver:    userSaver,
+		usrProvider: userProvider,
+		appProvider: appProvider,
+		tokenTTL:    tokenTTL,
+	}
 }
