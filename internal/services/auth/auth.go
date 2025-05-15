f93746c59eb3ecec12c3b5a88458c3f8b4e5d3bf
finlleyl <ilya_chibugaev@mail.ru>
2025-05-15 08:49:38 +0300

    add service layer

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -1,7 +1,23 @@
 package auth
 
-import "errors"
+import (
+	"context"
+	"errors"
+	"github.com/finlleyl/gRPC/internal/domain/models"
+)
 
 var (
 	ErrInvalidCredentials = errors.New("invalid credentials")
 )
+
+type UserSaver interface {
+	SaveUser(ctx context.Context, email string, passHash []byte) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (models.User, error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appID int) (models.App, error)
+}
